05c48c6b81c4b94ab87c4eb05cfa026e46b55f8c
Kaushik Bandyopadhyay <kaushik-b@hcl.com>
2021-07-11 16:20:04 +0530

    Basic Fungible Token: - Added a smart contract with a basic implementation of ERC20 standard; - Added openzeppelin contracts as dev dependencies; - Updated truffle config to add network and conpiler configurations; - Updated gitignore.

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+// migrations/2_deploy.js
+const FToken = artifacts.require('FToken');
+
+module.exports = async function (deployer) {
+  await deployer.deploy(FToken, 'FToken', 'FT', '10000000000000000000000');
+};
